22e5e25648b2b5eeda0fcfccfb39a41a327ed889
Siu Tung Pang <d00230302@dmail.dixie.edu>
2020-10-07 17:12:03 -0600

    First commit

---
+++ example.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0817d428a6fb68645"
+  instance_type = "t2.micro"
+}
